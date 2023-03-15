4f228cd4e616be1a5d61dfd032b71ea53de259ce
Mahmoud Magdy <126895837+MahmoudMagdy001@users.noreply.github.com>
2023-03-15 21:12:58 +0200

    last

--- lib/core/utlis/styles.dart
+++ lib/core/utlis/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 
 abstract class Styles {
   static const titleMedium = TextStyle(
-    fontSize: 20.0,
+    fontSize: 19.0,
     fontWeight: FontWeight.w600,
   );
 }
