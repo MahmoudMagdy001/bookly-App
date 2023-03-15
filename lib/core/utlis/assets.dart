97c2fe6d9aeafc1c380ff6edb3d945ac8cc40376
Mahmoud Magdy <126895837+MahmoudMagdy001@users.noreply.github.com>
2023-03-15 20:39:46 +0200

    last

--- lib/core/utlis/assets.dart
+++ lib/core/utlis/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
   static const search = 'assets/images/icsearch.png';
+  static const test = 'assets/images/test_image.png';
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,7 @@
-import 'package:bookly/core/utlis/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,6 +11,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
