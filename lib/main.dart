fcf4785dcdd2f4866152dbb49109e4739f5fbbac
Mahmoud Magdy <126895837+MahmoudMagdy001@users.noreply.github.com>
2023-03-16 19:43:58 +0200

    last ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utlis/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
